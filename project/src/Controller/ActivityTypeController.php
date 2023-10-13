95ff1806389d4158cd17cd2ca09193488113f1a8
Nikoquattro <nikoquattro@gmail.com>
2023-10-14 00:01:25 +0200

    feat (elements): CRUD pour ActivityType

--- project/src/Controller/ActivityTypeController.php
+++ project/src/Controller/ActivityTypeController.php
@@ -3,13 +3,16 @@
 namespace App\Controller;
 
 use App\Entity\ActivityType;
-use App\Repository\ActivityTypeRepository;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
+use App\Repository\ActivityTypeRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
 use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ActivityTypeController extends AbstractController
 {
@@ -41,5 +44,49 @@ class ActivityTypeController extends AbstractController
         $em->flush();
 
         return new JsonResponse(null, Response::HTTP_NO_CONTENT);
+    }
+
+    #[Route('/api/activity-type', name: 'api_activity_type_post', methods: ['POST'])]
+    public function createActivityType(
+        Request $request,
+        SerializerInterface $serializer,
+        EntityManagerInterface $em,
+        UrlGeneratorInterface $urlGenerator
+    ): JsonResponse {
+        $activityType = $serializer->deserialize($request->getContent(), ActivityType::class, 'json');
+        $em->persist($activityType);
+        $em->flush();
+
+        $jsonActivityType = $serializer->serialize($activityType, 'json', ['groups' => 'getActivityType']);
+
+        $location = $urlGenerator->generate(
+            'api_detail_activity_type_get',
+            [
+                'id' => $activityType->getId()
+            ],
+            UrlGeneratorInterface::ABSOLUTE_URL
+        );
+
+        return new JsonResponse($jsonActivityType, Response::HTTP_CREATED, ["Location" => $location], true);
+    }
+
+    #[Route('/api/activity-type/{id}', name: 'api_activity_type_put', methods: ['PUT'])]
+    public function updateActivityType(
+        Request $request,
+        SerializerInterface $serializer,
+        ActivityType $activityType,
+        EntityManagerInterface $em
+    ): JsonResponse {
+        $updatedActivityType = $serializer->deserialize(
+            $request->getContent(),
+            ActivityType::class,
+            'json',
+            [AbstractNormalizer::OBJECT_TO_POPULATE => $activityType]
+        );
+
+        $em->persist($updatedActivityType);
+        $em->flush();
+
+        return new JsonResponse(null, JsonResponse::HTTP_NO_CONTENT);
     }
 }
